82b3f9079fcd5236ca5f1c843d4d141b5046fd32
Luke Marren <lukemarren173@gmail.com>
2024-05-09 18:50:37 -0500

    Begin OLS model

--- R/data_cleaning.R
+++ R/data_cleaning.R
@@ -5,6 +5,11 @@ data = read.csv(file = "data/diet_data.csv", header = TRUE, sep = ",")
 View(data)
 glimpse(data)
 
+################################################################################
+## Data wrangling
+################################################################################
+
+## Add analysis variables
 data = data |>
   replace(is.na(data), 1) |>
   mutate(body_fat = (weight - muscle_mass - bone_mass),
@@ -14,7 +19,17 @@ data = data |>
          unsat_fat = (total_fat - sat_fat),
          fat_pct = ((total_fat*9)/caloric_intake),
          carb_pct = ((total_carb*4)/caloric_intake),
-         protein_pct = ((protein*4)/caloric_intake))
+         protein_pct = ((protein*4)/caloric_intake),
+         non_fiber_complex_carb = (total_carb - fiber - sugar))
 
-View(data)
+## Isolate regressors
+regressors_data = data |>
+  select(weight, muscle_mass, bone_mass, body_fat, unsat_fat, sat_fat,
+         non_fiber_complex_carb, fiber, sugar, protein, plyo, weightlift_times,
+         weightlift_type, basketball, swimming, core)
 
+ols_model = lm(weight ~ muscle_mass + bone_mass + body_fat + unsat_fat + sat_fat +
+   non_fiber_complex_carb + fiber + sugar + protein + plyo + weightlift_times +
+   weightlift_type + basketball + swimming + core, data)
+
+summary(ols_model)
