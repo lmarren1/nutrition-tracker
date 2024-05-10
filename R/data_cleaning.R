c00ed467067916dc290bb7b24f1857794c0f1225
Luke Marren <lukemarren173@gmail.com>
2024-05-10 16:29:39 -0500

    Create Fake Data for OLS modeling

--- R/data_cleaning.R
+++ R/data_cleaning.R
@@ -1,9 +1,28 @@
 library(tidyverse)
+library(ggplot2)
 
 data = read.csv(file = "data/diet_data.csv", header = TRUE, sep = ",")
 
 View(data)
 glimpse(data)
+
+data = data.frame(weight = sample(176:189, replace = TRUE, 30),
+                     muscle_mass = sample(150:154.5, replace = TRUE, 30),
+                     bone_mass = sample(7.8:8, replace = TRUE, 30),
+                     body_fat = sample(20:24, replace = TRUE, 30),
+                     unsat_fat = sample(15:70, replace = TRUE, 30),
+                     sat_fat = sample(15:30, replace = TRUE, 30),
+                     non_fiber_complex_carb = sample(100:200, replace = TRUE, 30),
+                     fiber = sample(15:50, replace = TRUE, 30),
+                     sugar = sample(10:60, replace = TRUE, 30),
+                     protein = sample(150:210, replace = TRUE, 30),
+                     plyo = sample(0:1, replace = TRUE, 30),
+                     weightlift_times = sample(1:2, replace = TRUE, 30),
+                     weightlift_type = sample(1:3, replace = TRUE, 30),
+                     basketball = sample(0:1, replace = TRUE, 30),
+                     swimming = sample(0:1, replace = TRUE, 30),
+                     core = sample(0:1, replace = TRUE, 30))
+
 
 ################################################################################
 ## Data wrangling
@@ -28,8 +47,18 @@ regressors_data = data |>
          non_fiber_complex_carb, fiber, sugar, protein, plyo, weightlift_times,
          weightlift_type, basketball, swimming, core)
 
-ols_model = lm(weight ~ muscle_mass + bone_mass + body_fat + unsat_fat + sat_fat +
-   non_fiber_complex_carb + fiber + sugar + protein + plyo + weightlift_times +
-   weightlift_type + basketball + swimming + core, data)
+ols_formula = data$weight ~ data$muscle_mass + data$bone_mass + data$body_fat + data$unsat_fat + data$sat_fat +
+  data$non_fiber_complex_carb + data$fiber + data$sugar + data$protein + data$plyo + data$weightlift_times +
+  data$weightlift_type + data$basketball + data$swimming + data$core
 
+ols_model = lm(ols_formula)
+ols_predictions = predict(ols_model, data)
 summary(ols_model)
+?summary
+summary.data.frame(data)
+
+ggplot() +
+  geom_point(mapping = aes(x = 1:30, y = data$weight)) +
+  geom_line(mapping = aes(x = 1:30, y = ols_predictions))
+
+
