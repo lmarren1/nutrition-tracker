a6fb83c7e53f0436d7fc83a4efc1bf60a87865d5
Luke Marren <lukemarren173@gmail.com>
2024-05-11 02:16:02 -0500

    Redo csv input data columns, prepare wrangling with fake data

--- R/data_cleaning.R
+++ R/data_cleaning.R
@@ -1,10 +1,54 @@
 library(tidyverse)
 library(ggplot2)
 
-data = read.csv(file = "data/diet_data.csv", header = TRUE, sep = ",")
+biometric_data = read.csv(file = "data/health_proj_biometric_data.csv",
+                          header = TRUE, sep = ",")
+nutrition_data = read.csv(file = "data/health_proj_nutrition_data.csv",
+                          header = TRUE, sep = ",")
+exercise_data = read.csv(file = "data/health_proj_exercise_data.csv",
+                         header = TRUE, sep = ",")
+performance_data = read.csv(file = "data/health_proj_performance_data.csv",
+                            header = TRUE, sep = ",")
 
-View(data)
-glimpse(data)
+biometric_data = data.frame(date = seq.Date(from = as.Date("2024/5/14"), by = 1, length.out = 30),
+                            weight_lbs = sample(178:191, replace = TRUE, 30),
+                            muscle_mass_lbs = sample(140:160, replace = TRUE, 30),
+                            bone_mass_lbs = sample(7:9, replace = TRUE, 30),
+                            sleep_hrs = sample(3:12, replace = TRUE, 30))
+View(biometric_data)
+
+nutrition_data = data.frame(date = seq.Date(from = as.Date("2024/5/14"), by = 1, length.out = 30),
+                            caloric_intake_kcal = sample(2400:3700, replace = TRUE, 30),
+                            total_fat_g = sample(20:140, replace = TRUE, 30),
+                            sat_fat_g = sample(0:40, replace = TRUE, 30),
+                            total_carb_g = sample(250:450, replace = TRUE, 30),
+                            fiber_g = sample(10:60, replace = TRUE, 30),
+                            sugar_g = sample(30:130, replace = TRUE, 30),
+                            protein_g = sample(160:210, replace = TRUE, 30))
+View(nutrition_data)
+
+exercise_data = data.frame(date = seq.Date(from = as.Date("2024/5/14"), by = 1, length.out = 30),
+                           plyo = sample(0:1, replace = TRUE, 30),
+                           weightlift_type = sample(1:3, replace = TRUE, 30),
+                           weightlift_volume_lbs = sample(3000:5500, replace = TRUE, 30),
+                           basketball_mins = sample(0:480, replace = TRUE, 30),
+                           swimming_mins = sample(0:80, replace = TRUE, 30),
+                           core = sample(0:1, replace = TRUE, 30))
+View(exercise_data)
+
+performance_data = data.frame(date = seq.Date(from = as.Date("2024/5/14"), by = 1, length.out = 30),
+                              standing_vertical_inches = sample(24:39, replace = TRUE, 30),
+                              max_vertical_inches = sample(25:40, replace = TRUE, 30),
+                              three_fourths_sprint_secs = sample(2.8:4.8, replace = TRUE, 30),
+                              lane_agility_secs = sample(10:18, replace = TRUE, 30))
+View(performance_data)
+
+
+
+
+
+
+
 
 data = data.frame(weight = sample(176:189, replace = TRUE, 30),
                      muscle_mass = sample(150:154.5, replace = TRUE, 30),
